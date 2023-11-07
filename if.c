50531f35ce419a39d9e348b456f6e49c79a2f5d4
Harshitasoni123 <150107107+Harshitasoni123@users.noreply.github.com>
2023-11-07 10:51:16 +0530

    Add files via upload

--- if.c
+++ if.c
@@ -1,11 +1,15 @@
-#include<stdio.h>
+#include <stdio.h>
 void main()
 {
- 
-
-
-
-
-
-
+ int num;
+ printf("\n Enter num value:");
+ scanf("%d",&num);
+ if(num<=55);
+ {
+  printf("%d is the greatest number",num);
+ }
+ if(num>=55);
+ {
+  printf("%d is the smallest number",num);
+ }
 }
