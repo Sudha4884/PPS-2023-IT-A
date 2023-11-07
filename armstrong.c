727eb6a7797bc960cbd25b30eb84661070b65901
Sathvika1209 <150106876+Sathvika1209@users.noreply.github.com>
2023-11-07 12:43:07 +0530

    Add files via upload

--- armstrong.c
+++ armstrong.c
@@ -1,25 +1,23 @@
 #include<stdio.h>
-void main(){
-
-
-int n, r, sum;
-
-printf("Enter the number: ");
-scanf("%d", &n);
-
+void main()
+{
+int n,r,sum;
+ 
+printf("Enter a number: ");
+scanf("\n %d",&n);
 int n1=n;
-while(n>0)
-{ 
- r=n%10;
- sum= sum+(r*r*r);
- n= n/10;
+
+while (n>0)
+{
+r=n%10;
+sum=sum+(r*r*r);
+n=n/10;
 }
 
-if (sum== n1)
- {
- printf("Given number is armstrong");
- }
-
-else 
-    printf("Given number is not armstrong");
- } 
+if (sum==n1)
+{ printf("The number is an armstrong.");
+}
+else
+{ printf("The number is not an armstrong.");
+}
+}
---
+++ size_1.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+void main()
+{
+float a=245.754;
+char s;
+printf("the sizes are %f,%c",sizeof(float),sizeof(char));
