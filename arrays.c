b0cf6cbf6020680ea3c0255bac7376035f1b04ba
AndoleSneha <150106421+AndoleSneha@users.noreply.github.com>
2023-11-07 11:03:43 +0530

    Add files via upload

--- arrays.c
+++ arrays.c
@@ -1,20 +1,13 @@
 #include<stdio.h>
 void main()
 {
-int A[10][10],r,c,i,j;
-printf("\n enter the no. of rows and cols");
-scanf("%d%d",&r,&c);
-printf("\n enter %d elements",r*c);
-for(i=0;i<r;i++)
-for(j=0;j<c;j++)
-scanf("%d",&A[i][j]);
-printf("\n******\n");
-for(i=0;i<r;i++)
+int marks[2]={0},i;
+for(i=0;i<2;i++)
 {
-for(j=0;j<c;j++)
-{
-printf("%d",A[i][j]);
+printf("\n Enter the  elements");
+scanf("%d",&marks[i]);
 }
-printf("\n");
-}
-}
+for(i=0;i<1;i++)
+printf("%4d", marks[i]);
+}           
+                  
