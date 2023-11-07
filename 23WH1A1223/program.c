8924b17f67ebf3ed1c16ef20ec2c56c42ea2b22f
23wh1a1223 <150107514+23wh1a1223@users.noreply.github.com>
2023-11-07 11:07:11 +0530

    Add files via upload

---
+++ 23WH1A1223/program.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+int a=2;
+int b=3;
+int c=4;
+int d=a+b+c;
+printf("\n%d",d);
+}
+
---
+++ 23WH1A1223/variables.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int num;
+char ch;
+printf ("\n plz give a character");
+scanf("%c",&ch);
+print("\n plz give a tntreger");
+scanf("%d",&num);
+printf("\n the charecter is %c",ch);
+printf("\n the integer is %d",num);
+float f;
+printf("\n plz give a float");
+scanf("%f",&f);
+}
