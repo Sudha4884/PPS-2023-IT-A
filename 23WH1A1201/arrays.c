ef45d4797b78a48bd9aa09d284153b749f52faeb
AndoleSneha <150106421+AndoleSneha@users.noreply.github.com>
2023-11-07 11:01:00 +0530

    Add files via upload

---
+++ 23WH1A1201/arrays.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int marks[2]={0},i;
+for(i=0;i<2;i++)
+{
+printf("\n Enter the  elements");
+scanf("%d",&marks[i]);
+}
+for(i=0;i<1;i++)
+printf("%4d", marks[i]);
+}           
+                  
