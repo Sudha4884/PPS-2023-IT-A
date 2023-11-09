27b86d7a54324c8af19db9aabf0e4e600fdf1e11
vaishnavi-1261 <150331457+vaishnavi-1261@users.noreply.github.com>
2023-11-09 14:45:13 +0530

    Add files via upload

---
+++ 23WH1A1261/factorial.c
@@ -0,0 +1,16 @@
+ // A  program to display the factiorial of a given integer//
+#include<stdio.h>
+int main()
+{
+int i,n;
+ int factorial=1;
+printf("entre a number to find factorial");
+scanf("%d",&n);
+for(i=1;i<=n;i++);
+{
+factorial=factorial*
+}
+printf("factorial of %d is %lld",n,fact);
+return 0;
+
+} 
