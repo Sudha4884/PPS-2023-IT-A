28a1b9d9a5dbc498f90ff4e84e6cb371a8bc7832
Nishatunnisa <150330807+Nishatunnisa@users.noreply.github.com>
2023-11-09 14:48:31 +0530

    Add files via upload

---
+++ 23WH1A1253/variables.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+int num;
+char ch;
+printf ("\n plz give a character");
+scanf("%c",&ch);
+printf("\n plz give a integer");
+scanf("%d",&num);
+printf("\n the character is %c",ch);
+printf("\n the integer is %d",num);
+float f;
+printf("\n plz give a float");
+scanf("%f",&f);
+double d;
+printf("\n plz give a double");
+scan("%d",&d);
+}
+
