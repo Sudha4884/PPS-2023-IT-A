345d4a63f8287f4f036fd68f07393db02acf2480
Nishatunnisa <150330807+Nishatunnisa@users.noreply.github.com>
2023-11-09 14:52:26 +0530

    Create array.c

---
+++ 23WH1A1253/array.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+void main()
+{
+int a[4][4],i,j,n,m;
+printf("enter number of rows and columns");
+scanf("%d%d",&m,&n);
+printf("enter the elements");
+for(i=0;i<m;i++)
+{ 
+for(j=0;j<n;j++)
+{
+scanf("%d",&a[i][j]);
+}
+ }
+for(i=0;i<m;i++)
+{
+for(j=0;j<n;j++)
+{
+printf("%3d",a[i][j]);
+}
+printf("\n");
+
+}
+}
