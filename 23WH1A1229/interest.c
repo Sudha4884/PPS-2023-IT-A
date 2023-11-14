ad3ecbb0e7c304cf46f3505252518c5c9a8c17ed
SaiGnanaLahari <150107098+SaiGnanaLahari@users.noreply.github.com>
2023-11-14 12:28:09 +0530

    Add files via upload

---
+++ 23WH1A1229/interest.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int P,T,R;
+int simple_interest;
+P=10000;
+T=20;
+R=10;
+simple_interest=(P*T*R)/100;
+printf("/n simple interest is %d",simple_interest);
+
+}
+
+
+
---
+++ 23WH1A1229/size.f
@@ -0,0 +1,28 @@
+#include<stdio.h>
+void main()
+{
+char ch='p';
+printf("size of char is %lu",sizeof(ch));
+
+
+
+int x;
+printf("size of int is %lu",sizeof(x));
+
+long a;
+printf("size of long is %lu",sizeof(a));
+
+double b;
+printf("size of double is %lu",sizeof(b));
+
+signed int d;
+printf("size of signed int is %lu",sizeof(d));
+
+unsigned int e;
+printf("size of unsigned int is %lu",sizeof(e));
+ 
+float g;
+printf("size of float is %lu",sizeof(g));
+
+}
+
