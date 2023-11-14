4cc7ebf20512d716054c7fc56317123916a06745
SaiGnanaLahari <150107098+SaiGnanaLahari@users.noreply.github.com>
2023-11-14 12:15:39 +0530

    Add files via upload

---
+++ 23WH1A1230/largest.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+int  main()
+{
+int a, b, c;
+printf("enter the values");
+scanf("%d%d%d",&a,&b,&c);
+//printf("%d%d%d",a,b,c);
+if(a>b)
+{
+if(a>c)
+printf("%d is greatest",a);
+else
+printf("%d is greatest",c);
+}
+else
+{
+if(b>c)
+
+printf("%d is greatest",b);
+else
+printf("%d is greatest",c);
+}
+return 0;
+
+
+}
+
+
+
+
+
+
+
+
+
+
