6aeddd630f6b7b80d0ad8a2d7febcf4d7fd62d06
SRISTI9 <150107100+SRISTI9@users.noreply.github.com>
2023-11-14 12:15:10 +0530

    Delete 23WH1A1206 directory

--- 23WH1A1206/addition2d.c
+++
@@ -1,5 +0,0 @@
-#include<stdio.h>
-void main()
-{
-inmt a[5][5],r,c,i,j;
-printf("enter rows and columns");
--- 23WH1A1206/opratorcs2.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-void main()
-{
- int x=3,a
- a=3+(4*5>3^4/++x);
- printf("\n a =%d",a);
-}
