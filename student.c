ef8ee4641ef16ec7898c18ae6cf76bcbe6ee00f4
23wh1a1221 <150107392+23wh1a1221@users.noreply.github.com>
2023-11-14 12:42:10 +0530

    Add files via upload

--- student.c
+++ student.c
@@ -1,17 +1,14 @@
-#include<string.h>
 #include<stdio.h>
 struct student
 {
 char name[20];
-int rno;
+int rollno;
 float cgpa;
 };
+struct student s1;
 void main()
 {
-struct student s1;
-puts("\n enter name,rno,cgpa");
-scanf("%s%d%f",s1.name,&s1.rno,&s1.cgpa);
-printf("\n NAME RNO CGPA\n");
-printf("\n %s %d %f",s1.name,s1.rno,s1.cgpa);
+printf("\n Enter name, rollno, cgpa");
+scanf("%s%d%f",s1.name, &s1.rollno, &s1.cgpa);
+printf("\n%s%d%f",s1.name, s1.rollno, s1.cgpa);
 }
-
