4291210b6d0318bd44d026843b1cfdc20797045a
Nishatunnisa <150330807+Nishatunnisa@users.noreply.github.com>
2023-11-15 05:34:05 -0800

    Create conversion.c

---
+++ 23WH1A1253/conversion.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main() 
+{
+  double value = 4150.333;
+  printf(" the double value %.2lf", value);
+  int valueb = 600.3444;
+  printf ("\n  the int value % d",valueb );
+  float values  = 34;
+  printf(" \n the float value %f", values);
+}
+ output:
+  the double value 4150.33
+  the int value  600 
+ the float value 34.000000
