b59b89a7e90223d4706a0d6c277cd83215dc871e
laharisiddireddygari <150107151+laharisiddireddygari@users.noreply.github.com>
2023-11-20 23:49:53 +0530

    Add files via upload

---
+++ 23WH1A1222/Arrays using pointers.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main() {
+    int data[5];
+    printf("Enter elements: ");
+    for (int i = 0; i < 5; ++i)
+    {
+        scanf("%d", data + i);
+    }
+    printf("You entered: \n");
+    for (int i = 0; i < 5; ++i)
+        printf("%d\n", *(data + i));
+}
