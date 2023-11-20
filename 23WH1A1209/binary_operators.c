3ff584b4b3b24c6249c00d04b3ab73147dbd1c6d
Sathvika1209 <150106876+Sathvika1209@users.noreply.github.com>
2023-11-20 20:24:05 +0530

    Create binary_operators.c

---
+++ 23WH1A1209/binary_operators.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+void main()
+{
+    int num, i;
+    int binary[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+    printf("Enter a number between 0 and 100: ");
+    scanf("%d", &num);
+    if (num < 0 || num > 100) {
+        printf("Invalid input\n");
+    }
+    for (i = 7; i >= 0; i--) {
+        if (num >= (1 << i)) {
+            binary[i] = 1;
+            num -= (1 << i);
+        }
+    }
+    printf("Binary equivalent: ");
+    for (i = 0; i < 8; i++) {
+        printf("%d", binary[i]);
+    }
+    printf("\n");
+}
