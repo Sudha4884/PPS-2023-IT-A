95d8fdd7ada8102fbfbcfc32030a656cf79f0bba
varshitha99999 <150106821+varshitha99999@users.noreply.github.com>
2023-11-21 07:52:19 +0530

    Create time_taken_byball.c

---
+++ 23WH1A1204/time_taken_byball.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include<math.h>
+
+int main() {
+    float s,t,u,a;
+    s=(10*3);
+    u=0.0;
+    a=9.8;
+    t=sqrt(2*s/a);
+    printf("time taken is %f",t);
+
+    return 0;
+}
