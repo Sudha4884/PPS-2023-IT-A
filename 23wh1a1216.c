eb7920e69c784369b9b22f09c919a2767319dd08
23wh1a1216 <150107269+23wh1a1216@users.noreply.github.com>
2023-11-28 12:31:48 +0530

    Add files via upload

---
+++ 23wh1a1216.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+void main()
+{ int a,b,c;
+  printf("a=");
+  printf("b=");
+  printf("c=");
+  scanf("%d %d %d",&a,&b,&c);
+ if(a>b)
+  { if(a>c)
+     printf("A is greater");
+  
+    else
+
+     printf("c is greater");
+  }
+else
+
+ {
+if(b>c) 
+printf ("b is greater");
+
+else
+ printf("c is greater");
+
+}
+}
