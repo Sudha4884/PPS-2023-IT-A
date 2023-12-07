1d97404be5cd638ab45e6883f2e438b28cffb1fd
Sree-Sindhu <150330738+Sree-Sindhu@users.noreply.github.com>
2023-12-07 14:51:30 +0530

    Add files via upload

---
+++ 23WH1A1251/readarray.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+void readArray(int x[],int no)
+{
+	int i;
+	printf("Enter %d number of elements: ",no);
+	for(i=0;i<no;i++)
+	{
+		scanf("%d",&x[i]);
+	}
+}
+void printArray(int x[],int no)
+{
+	int i;
+	for(i=0;i<no;i++)
+	{
+		printf("%d  ",x[i]);
+	}
+}
+void sumArray(int x[],int no)
+{
+	int i,sum=0;
+	for(i=0;i<no;i++)
+	{
+		sum = sum+x[i];
+	}
+	printf("\nSum is %d",sum);
+}
+void searchArray(int x[],int no)
+{
+	int i,k;
+	printf("\nEnter k: ");
+	scanf("%d",&k);
+	for(i=0;i<no;i++)
+	{
+		if (k == x[i])
+		{
+			printf("%d is present at %d index",k,i);
+			break;
+		}
+		else
+		{
+			printf("%d is not present",k);
+			break;
+		}
+	}
+}
+void main()
+{
+	int A[10],n;
+	printf("Enter n: ");
+	scanf("%d",&n);
+	readArray(A,n);
+	printArray(A,n);
+	sumArray(A,n);
+	searchArray(A,n);
+}
