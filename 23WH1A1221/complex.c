5d7aa9c77cb777b4e911d509c104aefe32132715
23wh1a1221 <150107392+23wh1a1221@users.noreply.github.com>
2023-12-14 16:11:04 +0530

    Add files via upload

---
+++ 23WH1A1221/complex.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+struct complex
+{
+int real;
+float imaginary;
+};
+struct complex com;
+void main()
+{
+printf("\n Enter real, imaginary");
+scanf("%s%d%d", com[i].real, &com[i].imaginary);
+printf("%s%d+%di", com[i].real, com[i].imaginary);
+}
