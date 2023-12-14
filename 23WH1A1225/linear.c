5300d6573311ed30fd5b00cd0f40d315c18d0d88
shailuboini <150106913+shailuboini@users.noreply.github.com>
2023-12-14 15:29:43 +0530

    Add files via upload

---
+++ 23WH1A1225/linear.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void printallprimes()
+void main()
+{
+   printallprimes(n);
+}
+void printallprimes(int n);
+printf("%d",n);
+
+
+}
+int i,count=0;
+for(i=2;i<=n;i++)
+{
+if(n%i==0) 
+count++;
+}
+if(count==2)
+printf("\n prime numbers");
+
+}
+
