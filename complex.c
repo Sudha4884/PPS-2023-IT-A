1d568a5cbe2743b9dc1914aed35dccfe782c8bc4
23wh1a1221 <150107392+23wh1a1221@users.noreply.github.com>
2023-12-14 16:05:47 +0530

    Add files via upload

--- complex.c
+++ complex.c
@@ -1,11 +1,13 @@
 #include<stdio.h>
 struct complex
-{ int real;
-  int imag;
+{
+int real;
+float imaginary;
 };
-int main()
-{ struct complex c1;
-printf("\n enter real and imag");
-scanf("%d%d",&c1.real,&c1.imag);
-printf("\n%d%d",c1.real,c1.imag);
+struct complex com;
+void main()
+{
+printf("\n Enter real, imaginary");
+scanf("%s%d%d", com[i].real, &com[i].imaginary);
+printf("%s%d+%di", com[i].real, com[i].imaginary);
 }
