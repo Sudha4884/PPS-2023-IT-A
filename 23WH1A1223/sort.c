b309e26e0757e65bed5266d2f868178be397725c
23wh1a1223 <150107514+23wh1a1223@users.noreply.github.com>
2023-12-14 16:03:01 +0530

    Add files via upload

---
+++ 23WH1A1223/sort.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+void array(int[],int);
+void printarray(int[],int);
+void insertionsort(int[],int);
+void main()
+{
+int a[10],i,n;
+printf("\n enter n");
+scanf("%d",&n);
+readarray(A,n);
+puts("\n");
+printarray(A,n);
+insertionsort(A,n);
+printarray(A,n);
+}
+void readarray(int A[], int noe)
+{
+int i;
+for(i=0;i<noe;i++)
+ scanf("%d",&A[i]);
+}
+void printarray(int A[],int noe)
+{
+int i;
+for(i=0;t<noe;i++)
+printf("%d",x[i]);
+}
+void insertion(int A[],int n);
+{ int i,j,ele;
+for(i=0;i<n;i++);
+{
+j=i-1;
+while((j>=) A& (A[j]>ele))
+{
+A[j+1] = A[i];
+j
