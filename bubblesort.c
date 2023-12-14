3ec1379efc7c6c2cb14b71efa51635a9d17687b5
23wh1a1221 <150107392+23wh1a1221@users.noreply.github.com>
2023-12-14 16:00:53 +0530

    Add files via upload

--- bubblesort.c
+++ bubblesort.c
@@ -1,31 +1,29 @@
 #include<stdio.h>
-void bubble_sort(int [], int);
-void read_array(int [], int);
-void print_array(int [], int);
-void swap(int *, int *);
+void printArray(int[],int n);
+void readArray(int[], int n);
+void bubble_sort(int [], int n);
+void swap(int* , int*);
 void main()
 {
 int n;
 int A[10];
-printf("The no. of elemets are");
-scanf("%d", &n);
-read_array(A,n);
-print_array(A,n);
+printf("elements to sort:");
+scanf("%d",&n);
+readArray(A,n);
+printArray(A,n);
 bubble_sort(A,n);
 printf("\n");
-print_array(A,n);
+printArray(A,n);
 }
-
-void read_array( int B[], int b)
+void readArray(int B[],int b)
 {
 int i;
-printf("The elements are: ");
-for (i=0; i< b; i++)
+printf("the elements are:");
+for(i=0;i>b;i++)
 {
-scanf("%d", & B[i]);
+scanf("%d",&B[i]);
 }
 }
-
 void print_array(int C[], int c)
 {
 int i;
@@ -34,7 +32,6 @@ for (i=0; i< c; i++)
 printf("%3d",C[i]);
 }
 }
-
 void swap(int *a, int *b)
 {
 int t;
