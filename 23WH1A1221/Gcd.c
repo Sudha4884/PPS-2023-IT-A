61c55c3e09f2add58b1de746e4b5c8b3f0b1f486
23wh1a1221 <150107392+23wh1a1221@users.noreply.github.com>
2023-12-19 19:49:59 +0530

    gcd.c

---
+++ 23WH1A1221/Gcd.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+ int a,b,i,gcd;
+ printf("enter 2 numbers :");
+ scanf("%d %d",&a,&b);
+ for(i=1;i<=a && i<=b;++i)
+ {
+     if(a%i==0 && b%i==0)
+     gcd = i;
+ }
+ printf("gcd of %d and %d is %d",a,b,gcd);
+
+    return 0;
+}
