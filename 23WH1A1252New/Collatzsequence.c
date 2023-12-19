b2c482049fe761488a1cadf518ba607bb863be3f
Poojitha Gangadhari <151541445+23WH1A1252@users.noreply.github.com>
2023-12-19 12:21:26 +0530

    Add files via upload

---
+++ 23WH1A1252New/Collatzsequence.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int collatz(int num)
+{
+int count=0;
+while(num>1)
+{
+if(num%2==0)
+{
+num=num/2;
+printf("\n%d",num);
+count++;
+}
+else
+{
+num=num*3+1;
+printf("\n%d",num);
+count++;
+}
+}
+return count;
+}
+void main()
+{
+int n1,cnt;
+printf("Enter a number:");
+scanf("%d",&n1);
+printf("collatz sequence from n1 %d",n1);
+cnt=collatz(n1);
+printf("Number of terms in the sequence %d",cnt);
+}
+
+
+
+
