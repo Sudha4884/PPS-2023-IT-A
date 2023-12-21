fe5e504c76ab2f7d5e0ed66a21ee2e320f769ce2
shailuboini <150106913+shailuboini@users.noreply.github.com>
2023-12-21 15:23:51 +0530

    Add files via upload

---
+++ 23WH1A1225/leapyear.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int LeapYear(int);
+int main()
+{
+int y;
+checkLeapYear(1980);
+printf("enter a number");
+scanf("%d",&y);
+checkLeapYear(y);
+}
+int checkLeapYear(int year)
+{
+if((year%400==0)||(year%4==0)&&(year%100!=0))
+printf("\nLEAP YEAR");
+else
+printf("\nNOT A LEAP YEAR");
+return 1;
+}
