1ba93eafcacbe4a552a4a5f8a71f1ee54e950a0d
poojitha1255 <150332062+poojitha1255@users.noreply.github.com>
2023-12-21 15:03:46 +0530

    Add files via upload

---
+++ 23WH1A1223/factorial.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int factorial(int);
+int main()
+{
+int n,f;
+printf("\n enter n:");
+scanf("%d",&n);
+f = factorial(n);
+printf("%d",f);
+return 0;
+}
+ int factorial(int n)
+{
+ if(n==0)
+ return 1;
+else
+ return n*factorial(n-1);
+}
---
+++ 23WH1A1223/fibonacci.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int fibonacci(int n)
+{
+ if(n==0)
+ return 0;
+ else if(n==1)
+ return 1;
+ else
+ return(fibonacci(n-1)+fibonacci(n-2));
+}
+ void main()
+{
+ int n,i;
+ printf("\n enter n:");
+ scanf("%d",&n);
+ for(i=0;i<=n;i++)
+ printf("%d",fibonacci(i));
+}
