0c3f125b5b8cfd3fd9ab4902deb5efac890287e8
vivardhanakudurupaka <150107001+vivardhanakudurupaka@users.noreply.github.com>
2023-12-21 09:41:29 +0000

    Add files via upload

---
+++ 23WH1A1211/fileintegers.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+FILE *fp;
+int i,n;
+fp = fopen("integers.txt","w+");
+for(i=1;i<11;i++)
+{
+puts("\n enter the number");
+scanf("%d",&n);
+putw(n,fp);
+}
+fclose(fp);
+fp=fopen("integers.txt","r+");
+for(i=1;i<11;i++)
+{
+
+printf("\n number = %d",getw(fp));
+
+}
+fclose(fp);
+}
+
+
+
