cdea005c316c482a78a8673c5399a5bce7658849
vivardhanakudurupaka <150107001+vivardhanakudurupaka@users.noreply.github.com>
2023-12-21 13:02:22 +0000

    Add files via upload

---
+++ 23WH1A1211/RecursionGDC.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int gcd(int a,int b);
+void main()
+{
+int a,b;
+printf("a = , b=");
+scanf("%d %d",&a,&b);
+printf("%d",gcd(a,b));
+}
+int gcd(int a,int b)
+{
+if(b==0)
+  return a;
+else
+  return gcd(b,a%b);
+ 
+}
