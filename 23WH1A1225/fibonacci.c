de0c356eb5d35d28f1658a2e6ce283f4bd2a1ed2
shailuboini <150106913+shailuboini@users.noreply.github.com>
2023-12-21 14:17:04 +0530

    Add files via upload

---
+++ 23WH1A1225/fibonacci.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+int fibonacci(int n);
+int main()
+{
+int n,i;
+scanf("%d",&n);
+for(i=0;i<n;i++)
+printf("%d",fibonacci(i));
+}
+int fibonacci (int n)
+{
+if (n==0)
+return 0;
+else if (n==1)
+return 1;
+else
+return(fibonacci(n-1)+fibonacci(n-2));
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
