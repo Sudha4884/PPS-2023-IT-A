518f1d9ab792c21758bec069cd4c2fe65a6b9c07
vivardhanakudurupaka <150107001+vivardhanakudurupaka@users.noreply.github.com>
2023-12-21 12:59:38 +0000

    Add files via upload

---
+++ 23WH1A1211/Recursionfibonacci.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int fibonacci(int n);
+void main()
+{
+int n,i;
+printf("enter number of terms");
+scanf("%d",&n);
+printf("fibonacci series is \n");
+for(i=0;i<n;i++)
+printf("%d",fibonacci(i));
+}
+int fibonacci(int n)
+{
+if(n==0)
+  return 0;
+else if (n==1)
+  return 1;
+else
+  return(fibonacci(n-1) + fibonacci(n-2));
+}
