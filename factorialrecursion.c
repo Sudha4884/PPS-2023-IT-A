9ec6b58f436fa2c5ea0b114c54a4d43f63d0a8fc
23wh1a1227 <150423367+23wh1a1227@users.noreply.github.com>
2023-12-21 13:47:01 +0530

    Add files via upload

---
+++ factorialrecursion.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int factorail(int);
+void main()
+{
+int n,f;
+printf("enter a number");
+scanf("%d", &n);
+f = factorial(n);
+printf("%d", f);
+}
+int factorial(int n)
+{
+if (n==0)
+return 1;
+else
+return n* factorial(n-1);
+}
