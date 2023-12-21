525b71121b1644473450696090bb3730fcb7aea8
23wh1a1223 <150107514+23wh1a1223@users.noreply.github.com>
2023-12-21 15:37:29 +0530

    Add files via upload

---
+++ 23WH1A1223/23wh1a1223/gcd.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int g(int,int);
+int main()
+{
+int a,b;
+printf("enter two numbers to find their gcd:");
+scanf("%d %d",&a,&b);
+int gcd=g(a,b);
+printf("\n gcd is %d",gcd);
+return 0;
+}
+int g(int a,int b)
+{
+int r;
+if(b!=0)
+{
+r=a%b;
+a=b;
+b=r;
+return g(a,b);
+}
+else
+{
+return a;
+}
+}
