5410321c931c9bdc07d605e35fa937ea3d399a7c
vivardhanakudurupaka <150107001+vivardhanakudurupaka@users.noreply.github.com>
2023-12-21 08:25:46 +0000

    Add files via upload

--- file.c
+++ file.c
@@ -1,38 +1,24 @@
 #include<stdio.h>
-void main ()
-
+#include<stdio.h>
+void main()
 {
-
-int num;
-char ch;
-float f;
-short int hi;
-long int li;
-long long int lld;
-double lf;
-long double LF;
-printf("\n please give a char");
-scanf("%c",&ch);
-printf("\n please give an integer");
-scanf("%d",&num);
-printf("\n please give float");
-scanf("%f",&f);
-
-printf("\n please give short int");
-scanf("%d",&hi);
-printf("\n please give long int");
-scanf("%d",&li);
-printf("\n please give long long int");
-scanf("%d",&lld);
-printf("\n please give double");
-scanf("%ld",&lf );
-printf("\n please give long double");
-scanf("%ld",&LF);
-
-printf("\nentered character is =%c \n integer=%d\n float=%f\n short int=%d\n long int=%d\n long long int=%d\n double=%ld\n long double=%ld",ch,num,f,hi,li,lld,lf,LF);
-
-
-
-
-
+FILE *fp;
+char ch,c;
+fp = fopen("my first file.txt","w");
+if(fp = NULL)
+{
+puts("\n problem is creating the file");
+exit(-1);
 }
+else 
+{
+puts("file created");
+ch = getchar();
+putc(ch,fp);
+fclose(fp);
+puts("\n the character got from the file is:");
+putchar(c);
+}
+}
+
+
