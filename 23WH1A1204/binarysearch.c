86834b0fc1bdba606adf856abb8012993ec5d998
varshitha99999 <150106821+varshitha99999@users.noreply.github.com>
2023-12-21 08:21:30 +0530

    Update binarysearch.c

--- 23WH1A1204/binarysearch.c
+++ 23WH1A1204/binarysearch.c
@@ -1,50 +1,56 @@
-#include<stdio.h>
-int BinarySearch(int [],int );
-void ReadArray(int [],int);
-void main()
-{
-  int n;int A[100];
-  printf("enter number of elements");
-  scanf("%d",&n);
-  printf("enter %d elements",n);
-  ReadArray(A,n);
-for(int i = 0;i<n;i++)
-{
-printf("%d",A[i]);
-} 
-int  a = BinarySearch(A,n);
-  printf("%d",a);
+#include <stdio.h>
+
+int BinarySearch(int A[], int num, int key);
+void ReadArray(int B[], int noe);
+
+int main() {
+    int n;
+    int A[100];
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    printf("Enter %d elements: ", n);
+    ReadArray(A, n);
+
+    printf("Entered array: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", A[i]);
+    }
+
+    int k;
+    printf("\nEnter element to be searched: ");
+    scanf("%d", &k);
+
+    int result = BinarySearch(A, n, k);
+
+    if (result != -1) {
+        printf("\nElement %d found at index %d.\n", k, result);
+    } else {
+        printf("\nElement %d not found.\n", k);
+    }
+
+    return 0;
 }
 
+void ReadArray(int B[], int noe) {
+    for (int i = 0; i < noe; i++) {
+        scanf("%d", &B[i]);
+    }
+}
 
-void ReadArray(int B[],int noe)
-  {
-    for (int i =0;i<noe;i++)
-      {
-        scanf("%d",&B[i]);
-      }
-  }
+int BinarySearch(int A[], int num, int key) {
+    int low = 0;
+    int high = num - 1;
 
-int BinarySearch(int A[],int num)
-   {
-      int k;
-       printf("enter element to be searched");
-       scanf("%d",&k);
-       int low = 0;
-       int high = num-1;
-       while( low <=  high)
-          {
-             
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        if (A[mid] == key) {
+            return mid;  // Element found
+        } else if (A[mid] < key) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
 
-             int mid = (low+high)/2;
-             if(A[mid] == k)
-             return mid;
-             else if(A[mid] < k)
-             low = mid+1;
-             else if(A[mid] > k)
-             high = mid-1;
-            
- } 
-
- }
-
+    return -1;  // Element not found
+}
