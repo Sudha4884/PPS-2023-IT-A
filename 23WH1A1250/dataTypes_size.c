2671965c7fd96b7628777269277f1e30993678fa
komarapuri-prashanthi <150332209+komarapuri-prashanthi@users.noreply.github.com>
2023-12-22 23:16:00 +0530

    dataTypes_size.c

---
+++ 23WH1A1250/dataTypes_size.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+//Size of variables of diffeerent variables
+int main() {
+    int a;
+    float b;
+    char c;
+    double d;
+    printf("size of integer = %d \n",sizeof(a));
+    printf("size of floaat = %d \n",sizeof(b));
+    printf("size of char = %d \n",sizeof(c));
+    printf("size of double = %d \n",sizeof(d));
+}
