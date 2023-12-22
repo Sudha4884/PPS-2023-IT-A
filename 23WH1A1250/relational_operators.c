ffad6d40ad06626a9962574d0e2cd84750daeb7a
komarapuri-prashanthi <150332209+komarapuri-prashanthi@users.noreply.github.com>
2023-12-22 23:53:21 +0530

    relational_operators.c

---
+++ 23WH1A1250/relational_operators.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+// relational operators
+int main() {
+    int a,b;
+    printf("enter values of a,b");
+    scanf("%d%d",&a,&b);
+    printf("a < b is %d \n",a<b);
+    printf("a > b is %d \n",a<b);
+    printf("a <= b is %d \n",a<=b);
+    printf("a >= b is %d \n",a>=b);
+    printf("a == b is %d \n",a==b);
+    printf("a != b is %d \n",a!=b);
+}
