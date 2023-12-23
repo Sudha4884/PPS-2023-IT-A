451215c9aa6bac3fa0aa85532d2e7d949bc7e9ba
23wh1a1216 <150107269+23wh1a1216@users.noreply.github.com>
2023-12-23 13:43:29 +0530

    Add files via upload

---
+++ 23WH1A1216/strong_number.c
@@ -0,0 +1,35 @@
+// strong number means sum of fact of each number
+
+#include<stdio.h>
+int Strong(int);
+
+void main()
+{int num;
+printf("enter a number");
+scanf("%d",&num);
+int a = Strong(num);
+if ( a==1)
+  printf("strong number");
+else
+  printf("not strong number");
+}
+  int Strong(int num)
+{
+   int fact=1,rem=0,i;
+   int a=0 ,b=num;
+while(num!=0)
+{
+rem=num%10;
+fact = 1;
+for( i=1;i<=rem;i++)
+{
+fact =fact*i;
+}
+a=fact+a;
+num=num/10;
+     }
+if(a==b)
+return 1;
+else
+return 0;
+}
