9cbb723b978fcec0de621c12fcedd6f3e8c2d3c8
23wh1a1215 <150107491+23wh1a1215@users.noreply.github.com>
2023-12-23 16:42:17 +0530

    Create calcu.c

---
+++ 23wh1a1215/calcu.c
@@ -0,0 +1,18 @@
+ #include <stdio.h>
+#include <math.h>
+
+int main() {
+    double x, result;
+
+    
+    printf("Enter the value of x: ");
+    scanf("%lf", &x);
+
+
+    result = 1 - (x / 2) + (pow(x, 2) / 4) - (pow(x, 3) / 6);
+
+    
+    printf("Result: %lf\n", result);
+
+    return 0;
+}
