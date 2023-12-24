e978b2909e6edb8f7261d98d3d1495888d3d23ea
Nishitha1248 <150331366+Nishitha1248@users.noreply.github.com>
2023-12-24 21:05:42 +0530

    Create area_diameter_0f circle.c

---
+++ 23WH1A1248/area_diameter_0f circle.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+// Area and Perimeter of rectangle
+int main() {
+    int r,diameter,area;
+    printf("enter value of r");
+    scanf("%d",&r);
+    diameter = 2*r;
+    area = 3.14*r*r;
+    printf("diameter of circle is = %d \n",diameter);
+    printf("area of circle is = %d",area);
+}
