7d7987755d759ca7e982d920f92f97a3d9cbdd70
Nerellamahathi <150332490+Nerellamahathi@users.noreply.github.com>
2023-12-24 12:37:45 +0530

    Add files via upload

---
+++ 23WH1A1211/sum_functions.c
@@ -0,0 +1,13 @@
+#include<stdio.h>>
+void sum()
+{ 
+int a,b,sum=0;
+	printf("enter the numbers a,b");
+	scanf("%d %d",&a,&b);
+	sum=a+b;
+	printf("the sum is %d",sum);
+}
+void main()
+ {
+ 	sum();
+ }
