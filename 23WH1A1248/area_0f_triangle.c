8088749dc8c61d37a91bfdaf18d330b3cc458b49
Nishitha1248 <150331366+Nishitha1248@users.noreply.github.com>
2023-12-25 10:33:24 +0530

    Create area_0f_triangle.c

---
+++ 23WH1A1248/area_0f_triangle.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+// Area of triangle
+int main() {
+    int l,b,h,area;
+    printf("enter values of l,b,h");
+    scanf("%d%d%d",&l,&b,&h);
+    area = l*b*h;
+    printf("area of triangle is = %d",area);
+}
