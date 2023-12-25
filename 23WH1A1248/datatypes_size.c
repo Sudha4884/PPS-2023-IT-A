a018d897fffd275d69e9ecf72578ef5ea3313ff1
Nishitha1248 <150331366+Nishitha1248@users.noreply.github.com>
2023-12-25 10:41:08 +0530

    Create datatypes_size.c

---
+++ 23WH1A1248/datatypes_size.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+//Size of diffeerent variables
+int main() {
+    printf("size of integer = %d \n",sizeof(int));
+    printf("size of float = %d \n",sizeof(float));
+    printf("size of char = %d \n",sizeof(char));
+    printf("size of double = %d \n",sizeof(double));
+}
