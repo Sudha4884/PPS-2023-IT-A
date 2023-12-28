4459b6faaa12695770cc9b364b74d1cb34bd8054
komarapuri-prashanthi <150332209+komarapuri-prashanthi@users.noreply.github.com>
2023-12-28 22:14:18 +0530

    simple&compound_interest.c

--- 23WH1A1250/simple_interest.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-// simple interest
-int main() {
-    int p,t,r,SI;
-    printf("enter p,t,r values: ");
-    scanf("%d%d%d",&p,&t,&r);
-    SI = p*t*r/100;
-    printf("simple interest is = %d \n",SI);
-}
