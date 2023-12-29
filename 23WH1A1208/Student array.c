fdd5d9a66a5953e75ba10ff4528cbd6f3960911b
malothuanjali <150107106+malothuanjali@users.noreply.github.com>
2023-12-29 17:06:32 +0530

    Create Student array.c

---
+++ 23WH1A1208/Student array.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+struct student
+{
+char name[5];
+int roll,cgpa;
+};
+void main()
+{
+int i;
+struct student std[5];
+for(i=0;i<5;i++)
+{
+printf("\nEnter name,Roll NUmber and CGPA");
+scanf("%s%d%d",std[i].name,&std[i].roll,&std[i].cgpa);
+}
+for(i=0;i<5;i++)
+{
+printf("\n %s  %s  %d",std[i].name,std[i].roll,std[i].cgpa);
+}
+}
