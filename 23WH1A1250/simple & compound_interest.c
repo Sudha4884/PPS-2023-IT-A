5a0b264906e63d793c69e0d35062cb9a387a30ec
komarapuri-prashanthi <150332209+komarapuri-prashanthi@users.noreply.github.com>
2024-01-01 19:14:08 +0530

    simple & compound_interest.c

--- 23WH1A1250/simple & compound_interest.c
+++ 23WH1A1250/simple & compound_interest.c
@@ -1,12 +1,8 @@
 #include <stdio.h>
-// simple interest
 int main() {
     float p,t,r,SI,amount,CI;
-    p=2.0;
-    t=7.0;
-    r=5.0;
-    // printf("enter p,t,r values: ");
-    // scanf("%d%d%d",&p,&t,&r);
+    printf("enter p,t,r values: ");
+    scanf("%f%f%f",&p,&t,&r);
     SI = p*t*r/100;
     amount = p * pow((1 + r/100),t);
     CI = amount - p;
