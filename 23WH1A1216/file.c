4e8c7b9f8f1f85162749654fef4d0f6981934397
23wh1a1216 <150107269+23wh1a1216@users.noreply.github.com>
2024-01-01 20:31:27 +0530

    Add files via upload

--- 23WH1A1216/file.c
+++ 23WH1A1216/file.c
@@ -1,38 +1,23 @@
 #include<stdio.h>
-void main ()
-
+#include<stdlib.h>
+int main()
+{
+FILE *fp;
+int i,n;
+fp = fopen("integers.txt","w+");
+for(i=1;i<11;i++)
+{
+puts("\n enter the number");
+scanf("%d",&n);
+putw(n,fp);
+}
+fclose(fp);
+fp=fopen("integers.txt","r+");
+for(i=1;i<11;i++)
 {
 
-int num;
-char ch;
-float f;
-short int hi;
-long int li;
-long long int lld;
-double lf;
-long double LF;
-printf("\n please give a char");
-scanf("%c",&ch);
-printf("\n please give an integer");
-scanf("%d",&num);
-printf("\n please give float");
-scanf("%f",&f);
+printf("\n number = %d",getw(fp));
 
-printf("\n please give short int");
-scanf("%d",&hi);
-printf("\n please give long int");
-scanf("%d",&li);
-printf("\n please give long long int");
-scanf("%d",&lld);
-printf("\n please give double");
-scanf("%ld",&lf );
-printf("\n please give long double");
-scanf("%ld",&LF);
-
-printf("\nentered character is =%c \n integer=%d\n float=%f\n short int=%d\n long int=%d\n long long int=%d\n double=%ld\n long double=%ld",ch,num,f,hi,li,lld,lf,LF);
-
-
-
-
-
+}
+fclose(fp);
 }
---
+++ 23WH1A1216/filemixed.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+void main()
+{
+FILE *fp;
+char a;
+int b;
+float c;
+fp = fopen("mixed.txt","w");
+scanf("%c %d %f",&a,&b,&c);
+fprintf(fp,"%c %d %f",a,b,c);
+fclose(fp);
+fp=fopen("mixed.txt","r");
+fscanf(fp,"%c %d %f",&a,&b,&c);
+printf("\n %c %d %f",a,b,c);
+fclose(fp);
+}
