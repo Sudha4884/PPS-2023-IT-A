74c2b0e1122fe13b2383c13a6df17d1092023155
DharaniYeluri <150106465+DharaniYeluri@users.noreply.github.com>
2024-01-02 13:12:29 +0530

    Add files via upload

---
+++ 23WH1A1228/palindromefunction.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int palindrome(int);
+int main()
+{
+int result,x;
+printf("Enter a number");
+scanf("%d",&x);
+result= palindrome(x);
+if (result==1)
+{printf("PALINDROME");
+}
+else
+{printf("NOT PALINDROME");
+}
+}
+int palindrome(int no)
+{
+int x,r,rev;
+int n=x;
+rev=0;
+while (x !=0)
+{
+r =n % 10;
+n= n/ 10;
+rev= (rev *10)+ r;
+}
+if (rev==n)
+return 1;
+
+}
